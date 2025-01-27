436c896308e22a2b907f02058f52f8849f16f52c
devanshrathod <devansh.rathod@hbwsl.com>
2025-01-27 19:36:21 +0530

    Creating test.php for rendering helloworld module

---
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Test.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mageplaza\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Test implements HttpGetActionInterface
+{
+    private ResultFactory $resultFactory;
+
+    public function __construct(ResultFactory $resultFactory)
+    {
+        $this->resultFactory = $resultFactory;
+    }
+
+    public function execute(): ResultInterface
+    {
+        /** @var \Magento\Framework\Controller\Result\Raw $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents("Hello World");
+
+        return $result;
+    }
+}
