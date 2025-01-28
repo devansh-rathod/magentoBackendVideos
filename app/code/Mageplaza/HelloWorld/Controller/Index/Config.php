9b0adbbb71b868bef3a198f19606c3592b84cfcd
devanshrathod <devansh.rathod@hbwsl.com>
2025-01-28 14:57:04 +0530

    Updating config.php to setContent from controller

--- app/code/Mageplaza/HelloWorld/Controller/Index/Config.php
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Config.php
@@ -4,28 +4,38 @@ namespace Mageplaza\HelloWorld\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Controller\ResultInterface;
 use Mageplaza\HelloWorld\Helper\Data as HelperData;
 
 class Config implements HttpGetActionInterface
 {
     private HelperData $helperData;
+    private ResultFactory $resultFactory;
 
     public function __construct(
+        ResultFactory $resultFactory,
         HelperData $helperData
     ) {
+        $this->resultFactory = $resultFactory;
         $this->helperData = $helperData;
     }
 
-    public function execute()
+    public function execute(): ResultInterface
     {
-        // Retrieve configuration values using the helper
+        // Retrieve configuration values
         $isEnabled = $this->helperData->getGeneralConfig('enable');
         $displayText = $this->helperData->getGeneralConfig('display_text');
 
-        // Output the configuration values
-        echo $isEnabled;
-        echo $displayText;
+        // Create response content
+        $content = sprintf(
+            "Enable: %s<br>Display Text: %s",
+            $isEnabled ? 'Yes' : 'No',
+            $displayText
+        );
 
-        return null;
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents($content);
+
+        return $result;
     }
 }
