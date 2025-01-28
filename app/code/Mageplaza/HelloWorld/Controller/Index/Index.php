91ed87fd4dda8da82d6aff9dc93a3136a5a92932
devanshrathod <devansh.rathod@hbwsl.com>
2025-01-28 11:41:15 +0530

    Using automatic constructor dependency injection

--- app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
+++ app/code/Mageplaza/HelloWorld/Controller/Index/Index.php
@@ -3,21 +3,34 @@
 namespace Mageplaza\HelloWorld\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\Controller\ResultFactory;
-use Magento\Framework\View\Result\PageFactory;
 use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Mageplaza\HelloWorld\Model\PostFactory;
 
 class Index implements HttpGetActionInterface
 {
     private PageFactory $pageFactory;
+    private PostFactory $postFactory;
 
-    public function __construct(PageFactory $pageFactory)
-    {
+    public function __construct(
+        PageFactory $pageFactory,
+        PostFactory $postFactory
+    ) {
         $this->pageFactory = $pageFactory;
+        $this->postFactory = $postFactory;
     }
 
     public function execute(): ResultInterface
     {
+        $post = $this->postFactory->create();
+        $collection = $post->getCollection();
+
+        foreach ($collection as $item) {
+            echo "<pre>";
+            print_r($item->getData());
+            echo "</pre>";
+        }
+
         return $this->pageFactory->create();
     }
 }
