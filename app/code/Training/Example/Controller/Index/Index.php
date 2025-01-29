8b644cff1d59cbd219ead3526b00827690dab44c
devanshrathod <devansh.rathod@hbwsl.com>
2025-01-29 17:45:06 +0530

    Updating Index.php Controller, Replacing direct die method with RawFactory for implementing Dependency Injection

--- app/code/Training/Example/Controller/Index/Index.php
+++ app/code/Training/Example/Controller/Index/Index.php
@@ -1,14 +1,26 @@
 <?php 
 
 declare(strict_types=1);
+
 namespace Training\Example\Controller\Index;
 
 use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\Result\RawFactory;
+use Magento\Framework\Controller\ResultInterface;
 
 class Index implements ActionInterface
 {
-    public function execute()
+    protected $resultFactory;
+
+    public function __construct(RawFactory $resultFactory)
     {
-        die('Hello World');
+        $this->resultFactory = $resultFactory;   
+    }
+
+    public function execute(): ResultInterface
+    {
+        $result = $this->resultFactory->create();
+        $result->setContents('Example');
+        return $result;
     }
 }
