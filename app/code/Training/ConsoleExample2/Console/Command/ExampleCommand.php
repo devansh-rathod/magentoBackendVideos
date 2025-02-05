03901158a04ebcea102eb494269a6d47544dae09
devanshrathod <devansh.rathod@hbwsl.com>
2025-02-05 14:30:00 +0530

    Creating Dependency Injection files and registration.php for our module ConsoleExample2 so that our component can get registered

---
+++ app/code/Training/ConsoleExample2/Console/Command/ExampleCommand.php
@@ -0,0 +1,25 @@
+<?php 
+namespace Training\ConsoleExample2\Console\Command;
+
+use Elasticsearch\Endpoints\Info;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExampleCommand extends Command
+{
+    protected function configure()
+    {
+        parent::configure();
+        $this->setName("training:example:run")
+             ->setDescription("Example command description");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Hello Devansh, this is a test command!');
+        $output->writeln('<info>Hello Devansh, this is a test command!</info>');
+        $output->writeln('<error>Hello Devansh, this is a test command!</error>');
+        $output->writeln('<question>Hello Devansh, this is a test command!</question>');
+    }
+}
