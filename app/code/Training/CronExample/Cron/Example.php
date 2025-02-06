62c5e06d3ba391c7948ea62386032f740c7b422f
devanshrathod <devansh.rathod@hbwsl.com>
2025-02-06 15:19:38 +0530

    Configuring crontab.xml to schedule cronjobs based on the 5 * and configuring them as required

--- app/code/Training/CronExample/Cron/Example.php
+++ app/code/Training/CronExample/Cron/Example.php
@@ -1,17 +1,16 @@
 <?php 
 namespace Training\CronExample\Cron;
-
-
 use Psr\Log\LoggerInterface;
-class Example{
-      protected LoggerInterface $logger;
-
-    public function __construct(LoggerInterface $logger){
-        $this->logger = $logger;
+class Example
+{
+    protected LoggerInterface $logger;
+    public function __construct(LoggerInterface $logger)
+    {
+      $this->logger = $logger;
     }
-    public function execute(){
-      $this->logger->info('Devansh ,cron job started');
-      sleep(2);
+    public function execute()
+    {
+      $this->logger->info('Devansh ,cron job started');  sleep(2);
       $this->logger->info('Devansh ,cron job ended');
     }
 }
